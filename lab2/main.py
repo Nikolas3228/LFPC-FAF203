b28d252b91f59299aeb1ab6f078fc690227adaeb
Sevcenco Nicolae <90716508+Nikolas3228@users.noreply.github.com>
2022-04-01 21:51:16 +0300

    NFAtoDFA remake

--- lab2/main.py
+++ lab2/main.py
@@ -1,92 +1,53 @@
-import pandas as pd
-
-grammar = [
-    '0 a 0',
-    '0 a 1',
-    '1 b 2',
-    '2 a 2',
-    '3 a 3',
-    '2 b 3'
-]
-
-#for each state we create a dictionary which is keyed by the letters of our alphabet {a,b}
-#and the global dictionary is keyed by the states of our NFA
-
-def parseGrammar(grammar):
-
-    nfa = {}
-    for grammarRules in grammar:
-        rulePart = grammarRules.split(' ')
-
-        if not rulePart[0] in nfa:
-            nfa[rulePart[0]] = {}
-
-        if not rulePart[1] in nfa[rulePart[0]]:
-            nfa[rulePart[0]][rulePart[1]] = ''
-
-        nfa[rulePart[0]][rulePart[1]] += rulePart[2]
-
-    return nfa
+NFA = {}
+inputs = ['a', 'b']
+states = ['q0', 'q1', 'q2', 'q3']
+transitions = [
+               ['q0', 'a', 'q0'], 
+               ['q0', 'a', 'q1'], 
+               ['q1', 'b', 'q2'], 
+               ['q2', 'a', 'q2'], 
+               ['q3', 'a', 'q3'],
+               ['q2', 'b', 'q3']
+               ]
 
 
-#the algorithm for converting NFA to DFA
-#we determine the set of states and values in nfa and add the new states that's length is bigger than 1
+for transition in transitions:
+    if transition[0] not in NFA.keys():
+        NFA[transition[0]] = []
+    NFA[transition[0]].append([transition[1], transition[2]])
 
-def NFAtoDFA(nfa):
 
-    states = []
-    values = []
-#add stated to new array
-    for state in nfa:
-        states.append(state)
+print ("Input", NFA)
 
-#check if new states exists
-    for state in nfa:
-        for value in nfa[state]:
+def check_states(str, set):
+    for c in str:
+        if ''.join(sorted(c)) == ''.join(sorted(set)):
+            return True
+    return False
 
-            if len(nfa[state][value]) > 1:
-                if not nfa[state][value] in states:
-                    states.append(nfa[state][value])
-            else:
-                if not nfa[state][value][0] in states:
-                    states.append(nfa[state][value][0])
-#check new transitions
-    for state in nfa:
-        for value in nfa[state]:
-            if not value in values:
-                values.append(value)
-#create an empty list for new states
+
+def NFAtoDFA():
     for state in states:
-        if not state in nfa:
-            newState = list(state)
-            for value in values:
-                val = []
+        if state in NFA.keys():
+            for input in inputs:
+                new_state = ''
+                adj_states = []
+                has_same_input = 0
+                for transition in NFA[state]:
+                    if input == transition[0]:
+                        has_same_input += 1
+                        new_state += transition[1]
+                        adj_states.append(transition[1])
+                if has_same_input > 1:
+                    if check_states(NFA.keys(), new_state):
+                        return
+                    NFA[new_state] = []
+                    for adj_state in adj_states:
+                        for trans in NFA[adj_state]:
+                            if trans not in NFA[new_state]: 
+                                NFA[new_state].append(trans)
+                    states.append(new_state)
 
-#for all the states that derive from the new state formated
-#we add the transitions to val list
-                for st in newState:
-                    if value in nfa[st]:
-                        val.append(nfa[st][value])
-#we add the new state in the global dict keyed by states
-                if not state in nfa:
-                    nfa[state] = {}
-#add the elements of the list to the inner dict which is keyed by alphabet elements
-#the values in the inner dict are joined as all the transitions from the formed state are considered
-                nfa[state][value] = ''.join(set(''.join(val)))
-                states.append(''.join(set(''.join(val))))
+NFAtoDFA()
 
-    return nfa
-
-print("The initial NFA")
-nfa = parseGrammar(grammar)
-print(nfa)
-NFA = pd.DataFrame(nfa)
-NFA = NFA.fillna("X")
-print(NFA.transpose())
-
-print("The DFA after conversion")
-dfa = NFAtoDFA(nfa)
-print(dfa)
-DFA = pd.DataFrame(dfa)
-DFA = DFA.fillna("X")
-print(DFA.transpose())
+print("DFA", NFA)
