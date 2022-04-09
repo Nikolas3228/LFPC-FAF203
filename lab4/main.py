a3fe2983c8d25573a25dbfb6472f6d78406d369c
Sevcenco Nicolae <90716508+Nikolas3228@users.noreply.github.com>
2022-04-09 14:53:28 +0300

    Add files via upload

--- lab4/main.py
+++ lab4/main.py
@@ -4,14 +4,13 @@ from itertools import combinations
 Vn = ['S','A','B','C','D']
 Vt = ['a','b']
   
-grRules = ['S->aB','S->bA','S->B','A->b','A->aD','A->AS','B->bAB','A->$','B->a','B->bS','C->AB','D->BB']
+productions = ['S->aB','S->bA','S->B','A->b','A->aD','A->AS','B->bAB','A->$','B->a','B->bS','C->AB','D->BB']
 
 def parseGrammar():
-
   grammar = {}
   
-  for tranz in grRules:
-    rules = tranz.split("->")
+  for transtions in productions:
+    rules = transtions.split("->")
     rule = list(rules[1])
     if not rules[0] in grammar:
       grammar[rules[0]] = []
@@ -21,114 +20,115 @@ def parseGrammar():
 
 parsedGrammar = parseGrammar()
 
-def check_eps(grammar):
-  eps_rules = []
+def checkEpsilon(grammar):
+  epsilon_rules = []
 
-  for lft_prod in grammar:
-    for rght_prod in grammar[lft_prod]:
-      if '$' in rght_prod and len(rght_prod) == 1:
-        eps_rules.append(lft_prod)
-  
-  return eps_rules
+  for state in grammar:
+    for transitions in grammar[state]:
+      if '$' in transitions and len(transitions) == 1:
+        epsilon_rules.append(state)
+  return epsilon_rules
 
 # Check the frequence of a non-terminal in a production.
-def check_frequence(rght_prod, nonTerm, symbol_freq):
+def checkFrequency(transitions, non_term, symbol_freq):
   freq = 0;
-  for index in range(len(rght_prod)):
-    if rght_prod[index] == nonTerm:
+  for index in range(len(transitions)):
+    if transitions[index] == non_term:
       freq += 1
       if freq == symbol_freq:
         return index
 
 # Create all possible subsets of a production.
-def subsets(rght_prod, remove_tranz, has_eps):
+def subsets(transitions, remove_transition, has_epsilon):
   # Init array for possible combinations
-  possible_comb = []
-  for x in range(1, has_eps + 1):
-    possible_comb += list(combinations(range(1, has_eps + 1), x))
+  possible_combinations = []
+  for x in range(1, has_epsilon + 1):
+    possible_combinations += list(combinations(range(1, has_epsilon + 1), x))
   
-
-  all_comb = []
-  for combination in possible_comb:
+  new_transitions = []
+  for combination in possible_combinations:
     # Copy the transition to compute.
-    comb = rght_prod.copy()
+    comb = transitions.copy()
     for number in combination:
       # Remove the empty state to create a new combination.
-      comb.pop(check_frequence(rght_prod, remove_tranz, number) - len(rght_prod) + len(comb))
+      comb.pop(checkFrequency(transitions, remove_transition, number) - len(transitions) + len(comb))
     # If the combination doesn't exists - add it to the list.
-    if comb not in all_comb:
-      all_comb.append(comb)
+    if comb not in new_transitions:
+      new_transitions.append(comb)
 
-  return all_comb
+  return new_transitions
 
-def remove_epsilon(grammar):
+def removeEpsilon(grammar):
     
-  while(len(check_eps(grammar))):
-    eps_rules = check_eps(grammar)
+  while(len(checkEpsilon(grammar))):
+    epsilon_rules = checkEpsilon(grammar)
     
-    for derives_eps in eps_rules:
-      for lft_prod in grammar:
-        for rght_prod in grammar[lft_prod]:
+    for derives_eps in epsilon_rules:
+      for state in grammar:
+        for transitions in grammar[state]:
           # Check if the empty state is in any right production.
-          if derives_eps in rght_prod:
+          if derives_eps in transitions:
             # Count how many empty states are in the production.
-            has_eps = rght_prod.count(derives_eps)
+            has_epsilon = transitions.count(derives_eps)
 
             # If all the rules derives to the empty state - make the current state empty.
-            if has_eps == len(rght_prod):
-              grammar[lft_prod].append(['$'])
+            if has_epsilon == len(transitions):
+              grammar[state].append(['$'])
               continue
 
             # Compute the possible combinations.
-            possibleCombinations = subsets(rght_prod, derives_eps, has_eps)
+            possibleCombinations = subsets(transitions, derives_eps, has_epsilon)
             # Update the grammar with the new transitions.
             for comb in possibleCombinations:
-              if comb not in grammar[lft_prod]:
-                grammar[lft_prod].append(comb)
+              if comb not in grammar[state]:
+                grammar[state].append(comb)
 
       # Remove epsilon transitions.
       grammar[derives_eps].remove(['$'])
   return grammar
 
-def remove_unit(grammar, Vn):
-  for lft_prod in grammar:
-    for rght_prod in grammar[lft_prod]:
+def removeUnit(grammar, Vn):
+  for state in grammar:
+    for transitions in grammar[state]:
       # Check if the states derives in a single Non-Terminal Symbol.
-      if len(rght_prod) == 1 and rght_prod[0] in Vn:
+      if len(transitions) == 1 and transitions[0] in Vn:
         # If true - remove it.
-        grammar[lft_prod].remove(rght_prod)
-        for rght_rule in grammar[rght_prod[0]]:
-          if rght_rule not in grammar[lft_prod]:
+        grammar[state].remove(transitions)
+        for transition_rules in grammar[transitions[0]]:
+          if transition_rules not in grammar[state]:
             # Update the current state' transition to the adj state transition.
-            grammar[lft_prod].append(rght_rule)
+            grammar[state].append(transition_rules)
   return grammar
 
-def remove_inaccessible(grammar, Vn):
-  nonTerms = Vn.copy();
-  nonTerms.remove("S")
+def removeInaccessible(grammar, Vn):
+  non_terminals = Vn.copy();
+  non_terminals.remove("S")
 
-  for nonTerminal in nonTerms:
-    isInaccessible = True
+  for non_terminal in non_terminals:
+    inaccessible = True
 
-    for lft_prod in grammar:
-      for rght_prod in grammar[lft_prod]:
-        if nonTerminal in rght_prod and lft_prod != nonTerminal:
-            isInaccessible = False
+    for state in grammar:
+      for transitions in grammar[state]:
+        if non_terminal in transitions and state != non_terminal:
+            inaccessible = False
             break
     
-    if isInaccessible:
-      grammar.pop(nonTerminal)
+    if inaccessible:
+      grammar.pop(non_terminal)
   return grammar
 
 
+
+print('\nInput', parsedGrammar['grammar'])
+
 print('\nStep 1: Eliminate epsilon productions')
-eliminate_eps_prod = remove_epsilon(parsedGrammar['grammar'])
+eliminate_eps_prod = removeEpsilon(parsedGrammar['grammar'])
 print(eliminate_eps_prod)
 
 print('\nStep 2: Eliminate unit productions')
-eliminate_unit_prod = remove_unit(eliminate_eps_prod, parsedGrammar['Vn'])
+eliminate_unit_prod = removeUnit(eliminate_eps_prod, parsedGrammar['Vn'])
 print(eliminate_unit_prod)
 
 print('\nStep 3: Eliminate inaccesible productions')
-eliminate_inaccessible_prod = remove_inaccessible(eliminate_unit_prod, parsedGrammar['Vn'])
+eliminate_inaccessible_prod = removeInaccessible(eliminate_unit_prod, parsedGrammar['Vn'])
 print(eliminate_inaccessible_prod)
